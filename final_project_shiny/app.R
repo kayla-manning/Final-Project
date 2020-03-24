5667fdb25e7f07815acf919aa0b183bd6e29e295
Kayla Manning <kaylamanning@college.harvard.edu>
2020-03-24 16:31:05 -0500

    added another tab with graphs on my shiny app

--- final_project_shiny/app.R
+++ final_project_shiny/app.R
@@ -5,9 +5,10 @@ library(shinythemes)
 # using the basic ui setup example from the textbook; will modify to fit my
 # unique project
 
+
 ui <- navbarPage(
   "HUDS Traffic Patterns in 2017-2018 and 2018-2019",
-  tabPanel("Model",
+  tabPanel("Change in Interhouse Swipes by Meal",
            fluidPage(theme = shinytheme("simplex"),
              titlePanel("Change in Interhouse Swipes by Meal"),
              sidebarLayout(
@@ -17,7 +18,21 @@ ui <- navbarPage(
                    "Meal",
                    c("Breakfast" = "bfast", "Lunch" = "lunch", "Dinner" = "dinner")
                  )),
-               mainPanel(imageOutput("Image")))
+               mainPanel(imageOutput("interhouse")))
+           )),
+  tabPanel("Daily Distributions of Fly-By Traffic",
+           fluidPage(theme = shinytheme("simplex"),
+                     titlePanel("Daily Distributions of Fly-By Traffic"),
+                     sidebarLayout(
+                       sidebarPanel(
+                         selectInput(
+                           "variable",
+                           "Year",
+                           c("2017-2018" = "1718", "2018-2019" = "1819",
+                             "2017-2018 and 2018-2019" = "both",
+                             "Increase" = "increase")
+                         )),
+                       mainPanel(imageOutput("daily_fly")))
            )),
   tabPanel("Discussion",
            titlePanel("Discussion Title"),
@@ -31,20 +46,37 @@ ui <- navbarPage(
 
 server <- function(input, output, session) {
 
-    # Send a pre-rendered image, and don't delete the image after sending it
+    # send a pre-rendered image, and don't delete the image after sending it
 
-    output$Image <- renderImage({
+    output$interhouse <- renderImage({
  
-         # When input$n is 3, filename is ./images/image3.jpeg
+         # will display appropriate image for input
  
          filename <- normalizePath(file.path('./change-interhouse-meal',
                                         paste('change_int_', input$meal, '.png', sep='')))
     
-    # Return a list containing the filename and alt text
+    # return a list containing the filename
   
           list(src = filename)
     
   }, deleteFile = FALSE)
+
+
+    # send a pre-rendered image, and don't delete the image after sending it
+    
+    output$daily_fly <- renderImage({
+
+      # will display appropriate image for input
+
+      filename <- normalizePath(file.path('./flyby-daily-counts',
+                                          paste(input$variable, '.png', sep='')))
+
+      # return a list containing the filename and alt text
+
+      list(src = filename)
+
+    }, deleteFile = FALSE)
+
 }
 
 # Run the application 
