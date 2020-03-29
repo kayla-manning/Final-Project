1e45d5ce30a6735b3b32d6bfbf1a5c00c4146ae7
Kayla Manning <kaylamanning@college.harvard.edu>
2020-03-29 14:15:55 -0500

    building up my shiny app some more ahead of milestone 6... would like to add some text to my 3rd graph tab before I submit

--- final_project_shiny/app.R
+++ final_project_shiny/app.R
@@ -7,37 +7,76 @@ library(shinythemes)
 
 
 ui <- navbarPage(
-  "HUDS Traffic Patterns in 2017-2018 and 2018-2019",
+  "HUDS Traffic Patterns in 2017-2018 and 2018-2019 Academic Years",
   tabPanel("Change in Interhouse Swipes by Meal",
            fluidPage(theme = shinytheme("simplex"),
              titlePanel("Change in Interhouse Swipes by Meal"),
              sidebarLayout(
                sidebarPanel(
+                 verticalLayout(
                  selectInput(
                    "meal",
                    "Meal",
                    c("Breakfast" = "bfast", "Lunch" = "lunch", "Dinner" = "dinner")
-                 )),
-               mainPanel(imageOutput("interhouse")))
+                 ),
+                 p("To calculate the data along the x-axis, I calculated the median 
+                    swipe counts for each category (interhouse, bagged meal, etc.), 
+                    house, and academic year in the data set. Then, I divided the 
+                    interhouse swipes for each house and year by the grand total of 
+                    swipes for each house and year. In doing so, I found the average 
+                    percentage of interhouse swipes out of total swipes for each house 
+                    and year. Then, I subtracted the 2017-2018 percentage from the 
+                    percentage in 2018-2019 to find the increase in percentage of 
+                    interhouse swipes between the two academic years. Select which
+                    meal you would like to view from the drop-down menu."))),
+               mainPanel(
+                   imageOutput("interhouse"),
+            ))
            )),
   tabPanel("Daily Distributions of Fly-By Traffic",
            fluidPage(theme = shinytheme("simplex"),
                      titlePanel("Daily Distributions of Fly-By Traffic"),
                      sidebarLayout(
                        sidebarPanel(
-                         selectInput(
-                           "variable",
-                           "Year",
-                           c("2017-2018" = "1718", "2018-2019" = "1819",
-                             "2017-2018 and 2018-2019" = "both",
-                             "Increase" = "increase")
-                         )),
-                       mainPanel(imageOutput("daily_fly")))
+                         verticalLayout(
+                           selectInput(
+                             "variable",
+                             "Year",
+                             c("2017-2018" = "1718", "2018-2019" = "1819",
+                               "2017-2018 and 2018-2019" = "both",
+                               "Increase" = "increase"
+                             )),
+                           p("The plot with the side-by-side swipe counts for the
+                             two academic years reveals that prior to the schedule
+                             change, Tuesdays and Thursdays were the most popular days
+                             at Fly-By. Following the schedule change, however,
+                             Wednesday and Thursday were the most popular. Wednesday
+                             saw the biggest increase in swipes between the two
+                             academic years."))),
+                       mainPanel(
+                         imageOutput("daily_fly")
+            ))
            )),
+  tabPanel("Comparing Interhouse and Resident Lunch Swipes",
+           fluidPage(theme = shinytheme("simplex"),
+                     titlePanel("Comparing Interhouse and Resident Lunch Swipes"),
+                     sidebarLayout(
+                       sidebarPanel(
+                         verticalLayout(
+                           selectInput(
+                             "type",
+                             "Type",
+                             c("Interhouse and Residents" = "both", 
+                               "Interhouse" = "int",
+                               "Residents" = "reg"
+                             )),
+                           p(""))),
+                       mainPanel(
+                         imageOutput("house_lunch")
+                       ))
+                         )),
   tabPanel("Discussion",
-           titlePanel("Discussion Title"),
-           p("Tour of the modeling choices you made and 
-             an explanation of why you made them")),
+           includeHTML(file.path("../discussion.html"))),
   tabPanel("About", 
            includeHTML(file.path("../about.html"))))
 
@@ -77,7 +116,24 @@ server <- function(input, output, session) {
 
     }, deleteFile = FALSE)
 
-}
+
+
+  # send a pre-rendered image, and don't delete the image after sending it
+  
+  output$house_lunch <- renderImage({
+    
+    # will display appropriate image for input
+    
+    filename <- normalizePath(file.path('./int-reg-lunch-houses',
+                                        paste('house_lunch_', input$type, '.png', sep='')))
+    
+    # return a list containing the filename and alt text
+    
+    list(src = filename)
+    
+  }, deleteFile = FALSE)
+  
+  }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
